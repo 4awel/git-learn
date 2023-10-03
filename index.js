29f671c365d91bc7f2c04d508c2a4f3b53f278bb
denissadrin@gmail.com <denisshadrin228@list.ru>
2023-10-03 09:46:27 +0300

    add if-else

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ function sayHi(say) {
     console.log(say)
 }
 
-sayHi('Hello world!')
+sayHi('Hello world!');
+
+function summ(a, b) {
+    return a + b;
+}
+
+const response = summ(10, 12);
+console.log(response);
+
+let c = 10;
+let d = 45;
+
+if (c < response) {
+    console.log('good!');
+} else {
+    console.log('error!')
+}
+
